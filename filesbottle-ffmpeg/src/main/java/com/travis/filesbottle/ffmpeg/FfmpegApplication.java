3743dfee00d56747990b5f5084c2ec7b49684b79
Travis1024 <travis_x@163.com>
2023-04-24 10:45:53 +0800

    update on 2023.4.24

--- filesbottle-ffmpeg/src/main/java/com/travis/filesbottle/ffmpeg/FfmpegApplication.java
+++ filesbottle-ffmpeg/src/main/java/com/travis/filesbottle/ffmpeg/FfmpegApplication.java
@@ -1,16 +1,20 @@
 package com.travis.filesbottle.ffmpeg;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @ClassName FfmpegApplication
- * @Description TODO
+ * @Description FfmpegApplication启动类
  * @Author travis-wei
  * @Version v1.0
  * @Data 2023/4/21
  */
+@EnableDubbo
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FfmpegApplication {
     public static void main(String[] args) {
--- filesbottle-ffmpeg/src/main/java/com/travis/filesbottle/ffmpeg/controller/VideoController.java
+++ filesbottle-ffmpeg/src/main/java/com/travis/filesbottle/ffmpeg/controller/VideoController.java
@@ -2,6 +2,7 @@ package com.travis.filesbottle.ffmpeg.controller;
 
 import cn.hutool.core.io.file.FileReader;
 import cn.hutool.core.util.StrUtil;
+import com.travis.filesbottle.ffmpeg.dubboimpl.DubboFfmpegServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +12,7 @@ import ws.schild.jave.process.ffmpeg.FFMPEGProcess;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.Objects;
 
 /**
  * @ClassName VideoController
@@ -30,27 +32,64 @@ public class VideoController {
     private String ffmpegPath;
     @Value("${ffmpeg.neturl}")
     private String ffmpegNetUrl;
+    @Value("${custom.ip}")
+    private String customIp;
+    @Value("${server.port}")
+    private String port;
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
 
 
+    /**
+     * @MethodName getPreviewUrl
+     * @Description 获取在线预览视频的 url 地址
+     * @Author travis-wei
+     * @Data 2023/4/24
+     * @param sourceId
+     * @Return java.lang.String
+     **/
+    @GetMapping("/getvideourl")
+    public String getPreviewUrl(@RequestParam("sourceId") String sourceId) {
+        return "http://" + customIp + ":" + port + contextPath + "/hlsvideo/video?sourceId=" + sourceId;
+    }
+
+
+    /**
+     * @MethodName getVideo
+     * @Description 在线预览视频信息
+     * @Author travis-wei
+     * @Data 2023/4/24
+     * @param sourceId
+     * @param response
+     * @Return void
+     **/
     @GetMapping("/video")
     public void getVideo(@RequestParam("sourceId") String sourceId, HttpServletResponse response) throws IOException {
         String filePath = videoFilePath + sourceId + "/" + sourceId + ".m3u8";
-        log.info(filePath);
         FileReader fileReader = new FileReader(filePath);
         log.info(filePath);
         fileReader.writeToStream(response.getOutputStream());
     }
 
-    @PostMapping("/delete")
-    public boolean deleteVideo(@RequestParam("sourceId") String sourceId) {
-        // TODO 删除 sourceId 对应的切片文件（ts）及相应的 m3u8 文件
-        return true;
+
+    /**
+     * @MethodName getHandleUrl
+     * @Description 获取上传视频文件的url 地址
+     * @Author travis-wei
+     * @Data 2023/4/24
+     * @param
+     * @Return java.lang.String
+     **/
+    @GetMapping("/gethandleurl")
+    public String getHandleUrl() {
+        return "http://" + customIp + ":" + port + contextPath + "/hlsvideo/handle";
     }
 
 
     /**
      * @MethodName sliceProcess
      * @Description 上传视频文件、转换格式成 mp4、并进行切片
+     * 访问路径：https://140.246.171.8:48086/hlsvideo/handle (post请求)
      * @Author travis-wei
      * @Data 2023/4/23
      * @param multipartFile
@@ -70,148 +109,7 @@ public class VideoController {
         // 获取视频文件的后缀
         String suffix = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
         // 创建文件夹（文件路径中已经包含了gridFsId）
-        // String dirPath = createDir(gridFsId);
-
-
-        // 临时存储的文件路径
-        String tempPath = videoFilePath + originalFilename;
-        // 最终mp4存储的文件路径
-        String filePath = videoFilePath + gridFsId + ".mp4";
-        // ts文件路径
-        String tsFilePath = videoFilePath + gridFsId + ".ts";
-        // m3u8视频索引路径
-        String m3u8FilePath = videoFilePath + gridFsId + ".m3u8";
-        // 最终视频切片文件存储的路径
-        String tsFinalFilePath = videoFilePath + gridFsId + "-%05d.ts";
-
-
-        /**
-         * 一、保存视频文件
-         */
-        InputStream inputStream = null;
-        FileOutputStream fileOutputStream = null;
-        try {
-            inputStream = multipartFile.getInputStream();
-            fileOutputStream = new FileOutputStream(tempPath);
-            IOUtils.copy(inputStream, fileOutputStream);
-            fileOutputStream.flush();
-
-        } catch (IOException e) {
-            log.error(e.getMessage());
-            return null;
-        } finally {
-            try {
-                if (inputStream != null) {
-                    inputStream.close();
-                }
-                if (fileOutputStream != null) {
-                    fileOutputStream.close();
-                }
-
-            } catch (Exception e) {
-                log.error(e.getMessage());
-                return null;
-            }
-        }
-
-        /**
-         * 二、判断视频文件是否为mp4的格式，不是则转为mp4
-         */
-        File tempFile = new File(tempPath);
-        if (suffix.equalsIgnoreCase("mp4")) {
-            // 不需要转换文件格式，只需要移动文件
-            tempFile.renameTo(new File(filePath));
-        } else {
-            boolean formatResult = formatToMp4(tempPath, filePath);
-            if (formatResult) tempFile.delete();
-        }
-
-        /**
-         * 三、进行切片操作（m3u8）
-         */
-        try {
-            // 将mp4格式的文件转换为ts格式
-            FFMPEGProcess ffmpeg = new FFMPEGProcess(ffmpegPath);
-            ffmpeg.addArgument("-y");
-            ffmpeg.addArgument("-i");
-            ffmpeg.addArgument(filePath);
-            ffmpeg.addArgument("-vcodec");
-            ffmpeg.addArgument("copy");
-            ffmpeg.addArgument("-acodec");
-            ffmpeg.addArgument("copy");
-            ffmpeg.addArgument("-vbsf");
-            ffmpeg.addArgument("h264_mp4toannexb");
-            ffmpeg.addArgument(tsFilePath);
-            ffmpeg.execute();
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(ffmpeg.getErrorStream()))) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                    log.info(line);
-                }
-            }
-            // 将ts文件进行切片
-            ffmpeg = new FFMPEGProcess(ffmpegPath);
-            ffmpeg.addArgument("-i");
-            ffmpeg.addArgument(tsFilePath);
-            ffmpeg.addArgument("-c");
-            ffmpeg.addArgument("copy");
-            ffmpeg.addArgument("-map");
-            ffmpeg.addArgument("0");
-            ffmpeg.addArgument("-f");
-            ffmpeg.addArgument("segment");
-            ffmpeg.addArgument("-segment_list");
-            ffmpeg.addArgument(m3u8FilePath);
-            // 设置每个切片的时长
-            ffmpeg.addArgument("-segment_time");
-            ffmpeg.addArgument("3");
-            ffmpeg.addArgument(tsFinalFilePath);
-            ffmpeg.execute();
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(ffmpeg.getErrorStream()))) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                    log.info(line);
-                }
-            }
-
-            /**
-             * 删除过程中保存的mp4、ts文件
-             */
-            new File(filePath).delete();
-            new File(tsFilePath).delete();
-
-        } catch (IOException e) {
-            log.error(e.getMessage());
-            return null;
-        }
-
-        return gridFsId;
-    }
-
-
-    /**
-     * @MethodName sliceProcess
-     * @Description 上传视频文件、转换格式成 mp4、并进行切片
-     * @Author travis-wei
-     * @Data 2023/4/23
-     * @param multipartFile
-     * @Return java.lang.String 返回视频文件的 gridFsId 以供判断请求是否成功
-     **/
-    @PostMapping("/handle2")
-    public String sliceProcessTwo(@RequestParam("file") MultipartFile multipartFile) {
-        // 获取文件的名称（gridFsId + 文件后缀）
-        String originalFilename = multipartFile.getOriginalFilename();
-        if (StrUtil.isEmpty(originalFilename)) {
-            return null;
-        }
-        // 获取文件的Context-type
-        String contentType = multipartFile.getContentType();
-        // 获取视频文件的gridFsId
-        String gridFsId = originalFilename.substring(0, originalFilename.lastIndexOf("."));
-        // 获取视频文件的后缀
-        String suffix = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
-        // 创建文件夹（文件路径中已经包含了gridFsId）
         String dirPath = createDir(gridFsId);
-
 
         // 临时存储的文件路径
         String tempPath = videoFilePath + originalFilename;
@@ -225,7 +123,6 @@ public class VideoController {
         String tsFinalFilePath = dirPath + gridFsId + "-%05d.ts";
         // 通过 nginx 访问切片的路径
         String nginxBaseUrl = ffmpegNetUrl + gridFsId + "/";
-
 
         /**
          * 一、保存视频文件
@@ -294,7 +191,7 @@ public class VideoController {
             ffmpeg.addArgument("0");
             // 设置每个切片的时间长度，单位 为秒
             ffmpeg.addArgument("-hls_time");
-            ffmpeg.addArgument("3");
+            ffmpeg.addArgument("2");
             ffmpeg.addArgument(m3u8FilePath);
             ffmpeg.execute();
             try (BufferedReader br = new BufferedReader(new InputStreamReader(ffmpeg.getErrorStream()))) {
@@ -318,6 +215,15 @@ public class VideoController {
     }
 
 
+    /**
+     * @MethodName formatToMp4
+     * @Description 将其他格式的视频格式转换为 mp4 的视频格式
+     * @Author travis-wei
+     * @Data 2023/4/24
+     * @param tempPath
+     * @param targetPath
+     * @Return boolean
+     **/
     private boolean formatToMp4(String tempPath, String targetPath) {
         try {
             FFMPEGProcess ffmpeg = new FFMPEGProcess(ffmpegPath);
@@ -364,6 +270,4 @@ public class VideoController {
 
         return targetPath;
     }
-
-
 }
